e41fbfcd8e78b637c5e6e9adbe7fed751a439cb4
leodayvison <leonardodayvison235@gmail.com>
2026-09-14 19:00:34 -0300

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+#!/usr/bin/env bash
+set -e
+
+# Cria o ambiente virtual caso ainda não exista
+if [ ! -d ".venv" ]; then
+    python3 -m venv .venv
+fi
+
+# Instala as dependências usando o pip isolado do venv
+.venv/bin/pip install --upgrade pip
+.venv/bin/pip install -r requirements.txt
+
 echo 'Ambiente configurado com sucesso! Lembre de sempre iniciar o ambiente virtual digitando "source .venv/bin/activate" e desativar ao terminar o trabalho, digitando "deactivate"'
